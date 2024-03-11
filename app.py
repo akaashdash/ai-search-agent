139164fae40ac271031350a894148a7cd519533b
akaashdash <akaash.dash@gmail.com>
2024-03-11 18:51:13 -0400

    integrate langchain into chainlit using groq inference api

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
+from langchain_groq import ChatGroq
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+
 import chainlit as cl
+
+@cl.on_chat_start
+async def start():
+    model = ChatGroq(temperature=0.25, model_name="mixtral-8x7b-32768")
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a helpful assistant."
+            ),
+            ("human", "{question}")
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
 
 @cl.on_message
 async def main(message: cl.Message):
-    await cl.Message(
-        content=f"Received: {message.content}"
-    ).send()
+    runnable = cl.user_session.get("runnable")
+    msg = cl.Message(content="")
+    async for chunk in runnable.astream(
+        { "question" : message.content },
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()])
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
